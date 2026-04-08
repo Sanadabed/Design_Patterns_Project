1909e900bb1b3600068dd4020e2eb716e25703eb
Sanadabed <sanadabed3333@gmail.com>
2026-04-08 22:23:30 +0300

    abstract factory

---
+++ Laptopstore/src/laptopstore/StudyLaptop.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laptopstore;
+
+/**
+ *
+ * @author Wafa
+ */
+public class StudyLaptop implements LaptopFactory {
+
+    @Override
+    public CPU createCPU() {
+        return null;
+    }
+
+    @Override
+    public RAM createRAM() {
+       return null;
+    }
+
+    @Override
+    public Storage createStorage() {
+       return null;
+    }
+
+    @Override
+    public GPU createGPU() {
+       return null;
+    }
+
+    @Override
+    public Screen createScreen() {
+        return null;
+    }
+    
+}
