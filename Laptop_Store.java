cb7ffad3d8246e64a7e15224f306b0df7cf1802e
Sanadabed <sanadabed3333@gmail.com>
2026-04-17 19:50:05 +0300

    main

---
+++ Laptop_Store.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laptopstore;
+
+/**
+ *
+ * @author kafaween
+ */
+public class Laptop_Store {
+
+    
+    public static void main(String[] args) {
+       
+   }   
+}
